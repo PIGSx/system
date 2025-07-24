4af4fc003cfd11fd9c01e2e248161fb60a54f592
pigs <hiury1315ajac@gmail.com>
2025-07-24 01:35:31 -0300

    test powerbi

--- system/src/pages/PowerBI/Geral/index.jsx
+++ system/src/pages/PowerBI/Geral/index.jsx
@@ -1,16 +1,16 @@
 const Geral = () => {
-    return (
-        <div>
+  return (
+    <div>
       <h1>Dashboard - Geral</h1>
       <iframe
-        title="Relatório Power BI - Geral"
-        width="100%"
-        height="600"
-        src="https://app.powerbi.com/home?experience=power-bi"
-        frameBorder="0"
-        allowFullScreen
+        title="J E H"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=274328ad-f3ed-473b-8161-57e71fedecb9&autoAuth=true&ctid=38ae2f02-5710-4e12-80bb-83600c3fdf1e"
+        frameborder="0"
+        allowFullScreen="true"
       ></iframe>
     </div>
-    )
-}
+  );
+};
 export default Geral;
